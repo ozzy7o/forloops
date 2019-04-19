a0fed21dafc89cc857e6e3fe3f7954d374c70c7f
Oz Parl <ozzy7o@icloud.com>
2019-04-18 20:57:10 -0400

    Add files via upload

---
+++ forloops/script.js
@@ -0,0 +1,28 @@
+
+console.log("print all numbers between -10 and 19")
+for(var i=-10; i<20; i+=1){
+    console.log(i);
+}
+console.log("print all even numbers between 10 and 40")
+for(var i=10; i<42; i+=2){
+    console.log(i);
+}
+
+console.log("print all odd numbers between 300 nd 333")
+for(var i=300; i<336; i+=3){
+    console.log(i);
+}
+console.log("print all odd numbers between 300 nd 333")
+
+for(var i=5; i<=50; i+=1){
+    
+    if(i % 5 === 0 && i% 3 === 0){
+    console.log(i);
+
+}
+}
+
+
+
+
+
